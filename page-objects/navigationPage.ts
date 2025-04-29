8715de3fd7310a52813e3e5e13e2e99fba0095db
OMER GUNDOGDU <omergundogdu75@gmail.com>
2025-04-30 00:14:57 +0300

    page object model

---
+++ page-objects/navigationPage.ts
@@ -0,0 +1,54 @@
+import { Page } from "@playwright/test";
+
+export class NavigationPage {
+
+    readonly page: Page
+
+    constructor(page: Page) {
+        this.page = page
+    }
+
+    async formLayoutPage(){
+        await this.selectGroupMenuItem('Forms');      
+          await this.page.getByText('Form Layouts').click();
+    }
+
+    async datepickerPage(){
+        await this.selectGroupMenuItem('Forms');
+        await this.page.getByText('Datepicker').click();
+    
+    }
+
+    async smartTablePage(){
+        await this.selectGroupMenuItem('Tables & Data');      
+        await this.page.getByText('Smart Table').click(); // 'Dialog' metnini bulup tıklıyoruz.
+    
+    }
+
+    async toastrPage(){
+        await this.selectGroupMenuItem('Modal & Overlays');      
+     
+        await this.page.getByText('Toastr').click();
+    }
+
+    async tooltipPage(){
+        await this.selectGroupMenuItem('Modal & Overlays');      
+
+        await this.page.getByText('Tooltip').click();
+    
+    }
+
+
+    private async selectGroupMenuItem(groupItemTitle: string){
+
+        const groupMenuItem = this.page.getByTitle(groupItemTitle);
+        const expandedState = await groupMenuItem.getAttribute('aria-expanded');
+
+        if(expandedState == 'false'){
+            await groupMenuItem.click();
+
+        }
+
+    }
+
+}
